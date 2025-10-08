6e086eb0d67f2c13843ce0273141d278b8b9070e
Gopi1311 <gopinathk1311@gmail.com>
2025-10-08 17:37:46 +0530

    relod

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -32,7 +32,7 @@ const Customers = () => {
   const getStatusColor = (status) => {
     return status === "Active"
       ? "bg-green-100 text-green-800"
-      : "bg-blue-100 text-blue-800";
+      : "bg-red-100 text-red-800";
   };
 
   const totalCustomer = customers.length;
