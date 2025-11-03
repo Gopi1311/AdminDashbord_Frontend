826fe9fe0c75b8f4239cec30b1958c76d6b412ee
Gopi1311 <gopinathk1311@gmail.com>
2025-11-03 13:02:36 +0530

    docker1

--- src/utils/BaseURL.js
+++ src/utils/BaseURL.js
@@ -1,6 +1,8 @@
 // export const BaseURL = 'https://admindashbord-backend.onrender.com/api/';
 // export const BaseURL = 'http://backend_lb:8081/api/';
-export const BaseURL = 'http://localhost:8081/api/';
+// export const BaseURL = 'http://localhost:8081/api/';
+export const BaseURL = 'https://docker-admin-render-yaml.onrender.com/api/';
+
 
 
 
